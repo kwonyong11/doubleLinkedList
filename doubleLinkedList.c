ed5c4874ecf3752a5b7880fe3c24d53262c7ee03
joint14 <joint14@123.com>
2020-07-23 15:44:59 +0900

    commit

--- doubleLinkedList.c
+++ doubleLinkedList.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <string.h>
-
+float sortScore[3] = {};
+char* sortName[3];
 struct Node {
     struct Node* before;
     char* name;
@@ -32,12 +33,29 @@ void addNode(struct Node* target, struct Node* tail,char* name, int data[]) {
            newNode->data[i] = data[i];
    }
 }
+void sort() {
+  int temp;
+  char* name;
+  for(int i =0; i<3; i++){
+    for(int j =0; j<3-i-1; j++) {
+      if(sortScore[j]<sortScore[j+1]) {
+        temp = sortScore[j];
+        sortScore[j] = sortScore[j+1];
+        sortScore[j+1] = temp;
+        name = sortName[j];
+        sortName[j] = sortName[j+1];
+        sortName[j+1] = name;
+      }
+    }
+  }
+}
 void printNode(struct Node* target) {
     int count = 0;
     struct Node* curr = (struct Node*)malloc(sizeof(struct Node));
     curr = target->next;
     int result =0;
     float aver=0;
+    int count_sort=0;
     char* sub[3] = {"국어","수학","영어"};
     while (curr->next!=NULL) {
         printf("\n");
@@ -49,11 +67,36 @@ void printNode(struct Node* target) {
         }
         printf("총점 : %d\t",result);
         aver = ((float)result)/3;
-        printf("평균 : %.2f",aver);
+        printf("평균 : %.2f\t",aver);
+        for(int i =0; i<3; i++) {
+          if(sortName[i] == curr->name) {
+            printf("등수 : %d",i+1);
+          }
+        }
         curr = curr->next;
         count++;
         printf("\n");
         result =0;
+    }
+}
+void count(struct Node* target) {
+    int count = 0;
+    struct Node* curr = (struct Node*)malloc(sizeof(struct Node));
+    curr = target->next;
+    int result =0;
+    float aver=0;
+
+    char* sub[3] = {"국어","수학","영어"};
+    while (curr->next!=NULL) {
+        for(int i =0; i<3;i++)
+          result += curr->data[i];
+        aver = ((float)result)/3;
+        sortScore[count] = aver;
+        sortName[count] = curr->name;
+        curr = curr->next;
+        printf("\n");
+        result =0;
+        count++;
     }
 }
 
@@ -83,5 +126,7 @@ void main() {
     for(int i=0; i<3; i++){
             addNode(head,tail,name[i],score[i]);
     }
+    count(head);
+    sort();
     printNode(head);
 }
