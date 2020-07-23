94a703d88aabca73eeee2da2c48d50874c06e7f4
Your Name <you@example.com>
2020-07-23 14:04:19 +0900

    add

--- doubleLinkedList.c
+++ doubleLinkedList.c
@@ -3,15 +3,38 @@
 #include <malloc.h>
 #include <string.h>
 
-struct Node {
-  struct Node* before;
+struct Node
+{
+  struct Node *before;
+  char* name;
   int data[3];
-  struct Node* next;
+  struct Node *next;
 };
 
-void addNode(struct Node* target, int* data);
-void printNode(struct Node* target);
+void addNode(struct Node *target, struct Node *tail,char* name, int *data[]);
+void printNode(struct Node *target);
 
-void main() {
-  
+void addNode(struct Node *target, struct Node *tail,char* name, int *data[])
+{
+  struct Node *newNode;
+  newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->name=name;
+  for (int i = 0; i < 3; i++)
+    {
+      newNode->data[i] = data[i];
+    }
+  if (tail->before == NULL)
+  {
+    newNode->next = tail->before;
+    newNode->before = target;
+    tail->before = newNode;
+    target->next = newNode;
+  }
+  else
+  {
+    target->next=newNode;
+    newNode->before = target;
+    newNode->next=tail->before;
+    tail->before=newNode;
+  }
 }
